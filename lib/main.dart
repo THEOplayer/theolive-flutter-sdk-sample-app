582b0543d62318b1905387f6e26fa220b255598e
Daniel Dallos <daniel.dallos@theoplayer.com>
2024-03-26 15:43:45 +0100

    removed unused plugin code

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-import 'package:flutter_theolive/flutter_theolive.dart';
-
 import 'movie_page.dart';
 
 
@@ -19,35 +14,10 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  final _flutterTheolivePlugin = FlutterTheolive();
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await _flutterTheolivePlugin.getPlatformVersion() ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
@@ -64,8 +34,6 @@ class _MyAppState extends State<MyApp> {
           FilledButton(onPressed: (){
             Navigator.push(context, MaterialPageRoute(builder: (context) =>  const MoviePage(title: "Movie",)));
           }, child: Text("Open Movie")),
-
-          Center(child: Text('Running on: $_platformVersion\n'),),
         ],)
 
       );
